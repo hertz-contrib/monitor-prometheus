3f2dff4dfcca83d2efe044a93a95b9b807f9da4b
smx_Moragn <1048492656@qq.com>
2024-10-11 18:12:22 +0800

    Connect to cwgo-pkg

--- trace.go
+++ trace.go
@@ -17,63 +17,14 @@
 package prometheus
 
 import (
-	"context"
-	"net/http"
-	"strconv"
-
-	"github.com/cloudwego/hertz/pkg/common/hlog"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
-
-	"github.com/cloudwego/hertz/pkg/app"
 	"github.com/cloudwego/hertz/pkg/common/tracer"
-	"github.com/cloudwego/hertz/pkg/common/tracer/stats"
-	prom "github.com/prometheus/client_golang/prometheus"
+
 	"github.com/prometheus/client_golang/prometheus/collectors"
+
+	"github.com/cloudwego-contrib/cwgo-pkg/telemetry/provider/promprovider"
+
+	"github.com/cloudwego-contrib/cwgo-pkg/telemetry/instrumentation/otelhertz"
 )
-
-const (
-	labelMethod     = "method"
-	labelStatusCode = "statusCode"
-	labelPath       = "path"
-
-	unknownLabelValue = "unknown"
-)
-
-// genLabels make labels values.
-func genLabels(ctx *app.RequestContext) prom.Labels {
-	labels := make(prom.Labels)
-	labels[labelMethod] = defaultValIfEmpty(string(ctx.Request.Method()), unknownLabelValue)
-	labels[labelStatusCode] = defaultValIfEmpty(strconv.Itoa(ctx.Response.Header.StatusCode()), unknownLabelValue)
-	labels[labelPath] = defaultValIfEmpty(ctx.FullPath(), unknownLabelValue)
-
-	return labels
-}
-
-type serverTracer struct {
-	serverHandledCounter   *prom.CounterVec
-	serverHandledHistogram *prom.HistogramVec
-}
-
-// Start record the beginning of server handling request from client.
-func (s *serverTracer) Start(ctx context.Context, c *app.RequestContext) context.Context {
-	return ctx
-}
-
-// Finish record the ending of server handling request from client.
-func (s *serverTracer) Finish(ctx context.Context, c *app.RequestContext) {
-	if c.GetTraceInfo().Stats().Level() == stats.LevelDisabled {
-		return
-	}
-
-	httpStart := c.GetTraceInfo().Stats().GetEvent(stats.HTTPStart)
-	httpFinish := c.GetTraceInfo().Stats().GetEvent(stats.HTTPFinish)
-	if httpFinish == nil || httpStart == nil {
-		return
-	}
-	cost := httpFinish.Time().Sub(httpStart.Time())
-	_ = counterAdd(s.serverHandledCounter, 1, genLabels(c))
-	_ = histogramObserve(s.serverHandledHistogram, cost, genLabels(c))
-}
 
 // NewServerTracer provides tracer for server access, addr and path is the scrape_configs for prometheus server.
 func NewServerTracer(addr, path string, opts ...Option) tracer.Tracer {
@@ -82,48 +33,18 @@ func NewServerTracer(addr, path string, opts ...Option) tracer.Tracer {
 	for _, opts := range opts {
 		opts.apply(cfg)
 	}
-
-	if !cfg.disableServer {
-		http.Handle(path, promhttp.HandlerFor(cfg.registry, promhttp.HandlerOpts{ErrorHandling: promhttp.ContinueOnError}))
-		go func() {
-			if err := http.ListenAndServe(addr, nil); err != nil {
-				hlog.Fatal("HERTZ: Unable to start a promhttp server, err: " + err.Error())
-			}
-		}()
-	}
-
-	serverHandledCounter := prom.NewCounterVec(
-		prom.CounterOpts{
-			Name: "hertz_server_throughput",
-			Help: "Total number of HTTPs completed by the server, regardless of success or failure.",
-		},
-		[]string{labelMethod, labelStatusCode, labelPath},
-	)
-	cfg.registry.MustRegister(serverHandledCounter)
-
-	serverHandledHistogram := prom.NewHistogramVec(
-		prom.HistogramOpts{
-			Name:    "hertz_server_latency_us",
-			Help:    "Latency (microseconds) of HTTP that had been application-level handled by the server.",
-			Buckets: cfg.buckets,
-		},
-		[]string{labelMethod, labelStatusCode, labelPath},
-	)
-	cfg.registry.MustRegister(serverHandledHistogram)
-
 	if cfg.enableGoCollector {
 		cfg.registry.MustRegister(collectors.NewGoCollector(collectors.WithGoCollectorRuntimeMetrics(cfg.runtimeMetricRules...)))
 	}
+	p := promprovider.NewPromProvider(
+		promprovider.WithRegistry(cfg.registry),
+		promprovider.WithHistogramBuckets(cfg.buckets),
+		promprovider.WithHttpServer(),
+	)
 
-	return &serverTracer{
-		serverHandledCounter:   serverHandledCounter,
-		serverHandledHistogram: serverHandledHistogram,
+	if !cfg.disableServer {
+		p.Serve(addr, path)
 	}
-}
 
-func defaultValIfEmpty(val, def string) string {
-	if val == "" {
-		return def
-	}
-	return val
+	return otelhertz.NewServerTracer()
 }
